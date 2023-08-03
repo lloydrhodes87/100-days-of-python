1994cfa3499485b40e83381d02e43c577f06e61d
Lloyd Rhodes <lloydrhodes@Lloyds-MacBook-Pro.local>
2023-08-03 09:07:07 +0100

    add directions key press to snake game

--- snake/main.py
+++ snake/main.py
@@ -11,10 +11,11 @@ screen.tracer(0)
 
 snake = Snake()
 
-# screen.listen(snake.up, 'Up')
-# screen.listen(snake.up, 'Up')
-# screen.listen(snake.up, 'Up')
-# screen.listen(snake.up, 'Up')
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 
 
--- snake/snake.py
+++ snake/snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
 STARTING_POSITIONS =  [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     """The snake class"""
@@ -8,6 +12,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         """creates the snake"""
@@ -26,4 +31,25 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """moves the snake up"""
+        print('here')
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """moves the snake down"""
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """moves the snake left"""
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """moves the snake right"""
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
