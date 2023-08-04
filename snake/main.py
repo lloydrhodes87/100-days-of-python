3f72476ee28073ad299bd91cc283b01a8fd5e8f8
Lloyd Rhodes <lloydrhodes@Lloyds-MacBook-Pro.local>
2023-08-04 09:09:51 +0100

    add score board to snake game

--- snake/main.py
+++ snake/main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from score_board import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 # Your turtle graphics code here
 
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -27,7 +31,9 @@ while game_is_on:
 
     snake.move()
    
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        score.increase_score()
 
 
 screen.exitonclick()
---
+++ snake/score_board.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+ALIGN="center"
+FONT=("Arial", 14, "normal")
+
+class Scoreboard(Turtle):
+    """A class that handles scores"""
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.penup()
+        self.goto((0, 280))
+        self.color("white")
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+
+
+    def increase_score(self):
+        self.clear()
+        self.score += 5
+        self.update_score()
